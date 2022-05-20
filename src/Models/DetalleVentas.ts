ab0ab1321af0b3a20508356a370461ca5187aa5b
AlexiSkyline <ilegal_sprite@outlook.com>
2022-05-20 17:37:32 -0500

    Agregamos el modelo de DetallesVentas

---
+++ src/Models/DetalleVentas.ts
@@ -0,0 +1,8 @@
+export class DetalleVentas {
+    id: string = "";
+    idVenta: string = "";
+    idArticulo: string = "";
+    cantidad: number = 0;
+    precioCompra: number = 0;
+    importe: number = 0;
+}
