4165fd9e8df886a0daace57e071ca011f629a101
AlexiSkyline <ilegal_sprite@outlook.com>
2022-05-20 17:53:55 -0500

    Agregamos las acciones para poder listar y borrar las ventas

--- src/app/detalle-venta/detalle-venta.component.ts
+++ src/app/detalle-venta/detalle-venta.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DetalleVentaService } from '../detalle-venta.service';
+import { DetalleVentas } from '../../Models/DetalleVentas';
 
 @Component({
   selector: 'app-detalle-venta',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detalle-venta.component.css']
 })
 export class DetalleVentaComponent implements OnInit {
-
-  constructor() { }
+  constructor( public DetalleVentaService: DetalleVentaService ) { }
 
   ngOnInit(): void {
+    this.DetalleVentaService.getDetalleVentasService();
   }
 
+  borrarDetalleVenta( data: DetalleVentas ) {
+    this.DetalleVentaService.eliminarDetalleVentaService( data );
+  }
 }
