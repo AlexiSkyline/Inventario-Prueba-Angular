ae1cec65fcc60799f0bb7a962a92f456057b67cf
AlexiSkyline <ilegal_sprite@outlook.com>
2022-05-20 16:51:54 -0500

    Agregamos la acción para agregar un nuevo articulo

--- src/app/articulo-agregar/articulo-agregar.component.ts
+++ src/app/articulo-agregar/articulo-agregar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ArticuloService } from '../articulo.service';
+import { Articulo } from '../../Models/Articulo';
 
 @Component({
   selector: 'app-articulo-agregar',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticuloAgregarComponent implements OnInit {
 
-  constructor() { }
+  constructor( public ArticuloService: ArticuloService ) { }
 
   ngOnInit(): void {
   }
 
+  guardarArticulo() {
+    this.ArticuloService.guardaArticuloService();
+  }
 }
