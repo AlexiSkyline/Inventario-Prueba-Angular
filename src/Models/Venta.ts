78444b9564dc10bc360e4d421fba2881ac69415f
AlexiSkyline <ilegal_sprite@outlook.com>
2022-05-20 17:39:44 -0500

    Agregamos el Modelo de Venta

---
+++ src/Models/Venta.ts
@@ -0,0 +1,12 @@
+export class Ventas {
+    id: string = "";
+    fecha: string = "";
+    idVendedor: string = "";
+    idCliente: string = "";
+    folio: number = 0;
+    idEmpresa: string = "";
+    total: number = 0;
+    iva: number = 0;
+    subTotal: number = 0;
+    pagoCon: string = "";
+}
