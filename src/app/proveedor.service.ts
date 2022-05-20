88552f20e1c2cb72aa7f8df836afa76000c689bf
AlexiSkyline <ilegal_sprite@outlook.com>
2022-05-20 12:17:11 -0500

    Agregamos el metodo para obtener los proveedores de la BD

--- src/app/proveedor.service.ts
+++ src/app/proveedor.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Proveedor } from '../Models/Proveedor';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProveedorService {
+    public listaProveedores: Proveedor[] = [];
+    public formData: Proveedor = new Proveedor;
+    public apiURL: string = 'https://localhost:7249/api/Proveedores';
+    public apiResponse: Proveedor = new Proveedor;
 
-  constructor() { }
+    constructor( public http: HttpClient ) { }
+
+    getProveedoresService() {
+        this.http.get( this.apiURL + '/ListarProveedores' )
+              .subscribe( data => {
+                  this.listaProveedores = data as Proveedor[];
+              });
+    }
 }
