b09ad7ad89271db98f9290d2b7631c79f62abf08
AlexiSkyline <ilegal_sprite@outlook.com>
2022-05-20 18:15:00 -0500

    Agregamos el metodo para obtener la lista de ventas realizadas

--- src/app/venta.service.ts
+++ src/app/venta.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Ventas } from '../Models/Venta';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VentaService {
+    public listaVentas: Ventas[] = [];
+    public formData: Ventas = new Ventas;
+    public apiURL: string = 'https://localhost:7249/api/Ventas';
+    public apiResponse: Ventas = new Ventas;
 
-  constructor() { }
+    constructor( public http: HttpClient ) { }
+
+    getVentasService() {
+        this.http.get( this.apiURL + '/ListarVentas' )
+              .subscribe( data => {
+                  this.listaVentas = data as Ventas[];
+              });
+    }
+
 }
