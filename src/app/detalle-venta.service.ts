8785cb7fad076f4a5d9a627ea7767a548f61b9b6
AlexiSkyline <ilegal_sprite@outlook.com>
2022-05-20 17:50:23 -0500

    Agregamos el metodo para obtener la lista de detalle ventas

--- src/app/detalle-venta.service.ts
+++ src/app/detalle-venta.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DetalleVentas } from '../Models/DetalleVentas';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DetalleVentaService {
+    public listaDetalleVentas: DetalleVentas[] = [];
+    public formData: DetalleVentas = new DetalleVentas;
+    public apiURL: string = 'https://localhost:7249/api/DetalleVentas';
+    public apiResponse: DetalleVentas = new DetalleVentas;
 
-  constructor() { }
+    constructor( public http: HttpClient ) { }
+
+    getDetalleVentasService() {
+        this.http.get( this.apiURL + '/ListarDetalleVentas' )
+              .subscribe( data => {
+                  this.listaDetalleVentas = data as DetalleVentas[];
+              });
+    }
 }
