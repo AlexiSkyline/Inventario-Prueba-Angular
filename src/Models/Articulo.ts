041e2a1000d614b320f4d7f13455274966e6ae39
AlexiSkyline <ilegal_sprite@outlook.com>
2022-05-20 13:39:46 -0500

    Agregamos el Modelo para  los articulos

---
+++ src/Models/Articulo.ts
@@ -0,0 +1,9 @@
+export class Articulo {
+    id: string = "";
+    nombre: string = "";
+    descripcion: string = "";
+    idUnidadMedida: string = "";
+    idMarca: string = "";
+    stock: number = 0;
+    idProveedor: string = "";
+}
